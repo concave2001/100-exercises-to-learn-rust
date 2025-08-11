6a0ee2f20b0f05e32249ceea27372bd458902886
concave2001 <160720@protonmail.com>
2025-08-11 18:06:01 +0300

    done some of the exercises

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut ret_val: u32 = 1;
+    let mut i: u32       = 1;
+    while i <= n {
+        ret_val *= i;
+        i += 1;
+    }
+
+    ret_val
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut ret_val: u32 = 1;
+
+    for i in 1..=n {
+        ret_val *= i;
+    }
+
+
+    ret_val
 }
 
 #[cfg(test)]
